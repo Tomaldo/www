52bebce836e5f882cbc50fddfa652f6fc5b0b87a
Tom Rune Ervik <tre@aksio.no>
2020-03-24 09:53:23 +0100

    Endringer

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -1818,24 +1818,24 @@ class PHPMailer
     protected static function isShellSafe($string)
     {
         // Future-proof
-        if (escapeshellcmd($string) !== $string
-            || !in_array(escapeshellarg($string), ["'$string'", "\"$string\""])
-        ) {
-            return false;
-        }
+        //if (escapeshellcmd($string) !== $string
+        //    || !in_array(escapeshellarg($string), ["'$string'", "\"$string\""])
+        //) {
+        //    return false;
+        //}
 
-        $length = strlen($string);
+        //$length = strlen($string);
 
-        for ($i = 0; $i < $length; ++$i) {
-            $c = $string[$i];
+        //for ($i = 0; $i < $length; ++$i) {
+          //  $c = $string[$i];
 
             // All other characters have a special meaning in at least one common shell, including = and +.
             // Full stop (.) has a special meaning in cmd.exe, but its impact should be negligible here.
             // Note that this does permit non-Latin alphanumeric characters based on the current locale.
-            if (!ctype_alnum($c) && strpos('@_-.', $c) === false) {
-                return false;
-            }
-        }
+            //if (!ctype_alnum($c) && strpos('@_-.', $c) === false) {
+              //  return false;
+            //}
+        //}
 
         return true;
     }
